05a4784a3ec1410a695bf2c1b6215453188ff33f
Phan Dang <phannhatdang@phans-macbook-air.local>
2019-02-17 11:25:08 +0700

    Initial Commit

---
+++ WeightTracker-Main/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeightTracker-Main
+//
+//  Created by Phan Nhat Dang on 2/17/19.
+//  Copyright © 2019 Phan Nhat Dang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
