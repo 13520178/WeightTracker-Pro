9f94d3793fc3218f0a94feb60afb4bf3acfab13f
Phan Dang <phannhatdang@phans-macbook-air.local>
2019-03-11 10:22:42 +0700

    complete design of BMI's ToolCell

--- WeightTracker-Main/Cells Custom/ToolCell.swift
+++ WeightTracker-Main/Cells Custom/ToolCell.swift
@@ -39,7 +39,7 @@ class ToolCell: BaseCell {
     
     let roundView:UIView = {
         let v = UIView()
-        v.backgroundColor = #colorLiteral(red: 0.5320518613, green: 0.2923432589, blue: 1, alpha: 1)
+        v.backgroundColor = #colorLiteral(red: 0.9764705896, green: 0.9293981611, blue: 0.7403491939, alpha: 1)
         v.layer.cornerRadius = 6.0
         v.layer.masksToBounds = true
         
@@ -56,6 +56,7 @@ class ToolCell: BaseCell {
     
     let cmLabel:UILabel = {
         let label = UILabel()
+        label.text = "kg"
         label.font = UIFont.systemFont(ofSize: 18)
         label.textColor = #colorLiteral(red: 0.5320518613, green: 0.2923432589, blue: 1, alpha: 1)
         return label
@@ -109,7 +110,14 @@ class ToolCell: BaseCell {
     let detailView:UIView = {
        let v = UIView()
         v.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        
+        return v
+    }()
+    
+    let progressView:UIView = {
+        let v = UIView()
+        v.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        v.layer.cornerRadius = 15.0
+        v.layer.masksToBounds = true
         return v
     }()
     
@@ -123,20 +131,83 @@ class ToolCell: BaseCell {
         return bt
     }()
     
-    let heightLabel:UILabel = {
+    let BMILabel:UILabel = {
         let label = UILabel()
-        label.text = "cm"
-        label.font = UIFont.systemFont(ofSize: 18)
-        label.textColor = #colorLiteral(red: 0.5320518613, green: 0.2923432589, blue: 1, alpha: 1)
+        label.text = "BMI"
+        label.font = UIFont.systemFont(ofSize: 24, weight: UIFont.Weight.bold)
+        label.textColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
         return label
     }()
-
     
+    let BMIValueLabel:UILabel = {
+        let label = UILabel()
+        label.text = "20.54"
+        label.font = UIFont.systemFont(ofSize: 52, weight: UIFont.Weight.bold)
+        label.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        return label
+    }()
+    
+    let kgValueLabel:UILabel = {
+        let label = UILabel()
+        label.text = "65 kg"
+        label.font = UIFont.systemFont(ofSize: 17, weight: UIFont.Weight.medium)
+        label.textColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+        return label
+    }()
+    
+    let cmValueLabel:UILabel = {
+        let label = UILabel()
+        label.text = "168 cm"
+        label.font = UIFont.systemFont(ofSize: 17, weight: UIFont.Weight.medium)
+        label.textColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+        return label
+    }()
+    
+    var kgCmStackView = UIStackView()
+
+    let lineDetailView:UIView = {
+        let v = UIView()
+        v.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+        return v
+    }()
+    
+    let categoryLabel:UILabel = {
+        let label = UILabel()
+        label.text = "Category"
+        label.font = UIFont.systemFont(ofSize: 16, weight: UIFont.Weight.medium)
+        label.textColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+        return label
+    }()
+    
+    let categoryValueLabel:UILabel = {
+        let label = UILabel()
+        label.text = "Normal (healthy weight)"
+        label.font = UIFont.systemFont(ofSize: 20, weight: UIFont.Weight.medium)
+        label.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        return label
+    }()
+    
+    let riskLabel:UILabel = {
+        let label = UILabel()
+        label.text = "Risks of diseases"
+        label.font = UIFont.systemFont(ofSize: 16, weight: UIFont.Weight.medium)
+        label.textColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+        return label
+    }()
+    
+    let riskValueLabel:UILabel = {
+        let label = UILabel()
+        label.text = "Medium"
+        label.font = UIFont.systemFont(ofSize: 20, weight: UIFont.Weight.medium)
+        label.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        return label
+    }()
     
     //MARK: - Setup View
     override func setUpView() {
         super.setUpView()
-        backgroundColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
+        backgroundColor = #colorLiteral(red: 1, green: 0.9446946864, blue: 0.7848783566, alpha: 1)
         setInputView()
         setDetailView()
         
@@ -150,7 +221,9 @@ class ToolCell: BaseCell {
         self.addGestureRecognizer(tap)
         
         if defaults.bool(forKey: "wasShowInput") == true {
+            print(self.profileView.center.y)
             setProfileViewPosition()
+            print(self.profileView.center.y)
         }
         
     }
@@ -161,6 +234,7 @@ class ToolCell: BaseCell {
     
     //MARK: - Setup Detail View
     func setDetailView(){
+        
         detailView.isHidden = true
         addSubview(detailView)
         detailView.translatesAutoresizingMaskIntoConstraints = false
@@ -169,9 +243,91 @@ class ToolCell: BaseCell {
         detailView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0).isActive = true
         detailView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0).isActive = true
         
-        //add profile label
-      
+        // add image to Detail View
+        let backgroundImage = UIImage(named: "toolCellBackground")
+        let backgroundView = UIImageView(image: backgroundImage)
+        backgroundView.contentMode = .scaleToFill
         
+
+        self.detailView.addSubview(backgroundView)
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundView.topAnchor.constraint(equalTo: detailView.topAnchor, constant: 0).isActive = true
+        backgroundView.leadingAnchor.constraint(equalTo: detailView.leadingAnchor, constant: 0).isActive = true
+        backgroundView.trailingAnchor.constraint(equalTo: detailView.trailingAnchor, constant: 0).isActive = true
+        backgroundView.bottomAnchor.constraint(equalTo: detailView.bottomAnchor, constant: 0).isActive = true
+        
+        self.detailView.addSubview(progressView)
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        progressView.topAnchor.constraint(equalTo: detailView.topAnchor, constant: 20).isActive = true
+        progressView.leadingAnchor.constraint(equalTo: detailView.leadingAnchor, constant: 16).isActive = true
+        progressView.trailingAnchor.constraint(equalTo: detailView.trailingAnchor, constant: -16).isActive = true
+        progressView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        
+        self.detailView.addSubview(BMILabel)
+        BMILabel.translatesAutoresizingMaskIntoConstraints = false
+        BMILabel.topAnchor.constraint(equalTo: progressView.bottomAnchor, constant: 24).isActive = true
+        BMILabel.centerXAnchor.constraint(equalTo: detailView.centerXAnchor).isActive = true
+        
+        self.detailView.addSubview(BMIValueLabel)
+        BMIValueLabel.translatesAutoresizingMaskIntoConstraints = false
+        BMIValueLabel.topAnchor.constraint(equalTo: BMILabel.bottomAnchor, constant: -4).isActive = true
+        BMIValueLabel.centerXAnchor.constraint(equalTo: detailView.centerXAnchor).isActive = true
+       
+        
+        //Set kgCmStackView
+        let kgLabelView = UIView()
+        let cmLabelView = UIView()
+        
+        kgCmStackView = UIStackView(arrangedSubviews: [kgLabelView,cmLabelView])
+        kgCmStackView.axis = .vertical
+        kgCmStackView.distribution = .fillEqually
+        
+        self.detailView.addSubview(kgCmStackView)
+        kgCmStackView.translatesAutoresizingMaskIntoConstraints = false
+        kgCmStackView.topAnchor.constraint(equalTo: BMIValueLabel.bottomAnchor, constant: -20.0).isActive = true
+        kgCmStackView.trailingAnchor.constraint(equalTo: detailView.trailingAnchor, constant: -12.0).isActive = true
+        kgCmStackView.widthAnchor.constraint(equalToConstant: 75.0).isActive = true
+        kgCmStackView.heightAnchor.constraint(equalToConstant: 50.0).isActive = true
+        
+        self.detailView.addSubview(kgValueLabel)
+        kgValueLabel.translatesAutoresizingMaskIntoConstraints = false
+        kgValueLabel.topAnchor.constraint(equalTo: kgLabelView.topAnchor, constant: 0.0).isActive = true
+        kgValueLabel.leadingAnchor.constraint(equalTo: kgLabelView.leadingAnchor, constant: 0.0).isActive = true
+
+        self.detailView.addSubview(cmValueLabel)
+        cmValueLabel.translatesAutoresizingMaskIntoConstraints = false
+        cmValueLabel.topAnchor.constraint(equalTo: cmLabelView.topAnchor, constant: 0.0).isActive = true
+        cmValueLabel.leadingAnchor.constraint(equalTo: cmLabelView.leadingAnchor, constant: 0.0).isActive = true
+        
+        self.detailView.addSubview(lineDetailView)
+        lineDetailView.translatesAutoresizingMaskIntoConstraints = false
+        lineDetailView.topAnchor.constraint(equalTo: kgCmStackView.bottomAnchor, constant: 8.0).isActive = true
+        lineDetailView.leadingAnchor.constraint(equalTo: detailView.leadingAnchor, constant: 32.0).isActive = true
+        lineDetailView.trailingAnchor.constraint(equalTo: detailView.trailingAnchor, constant: -32.0).isActive = true
+        lineDetailView.heightAnchor.constraint(equalToConstant: 1.0).isActive = true
+        
+        self.detailView.addSubview(categoryLabel)
+        categoryLabel.translatesAutoresizingMaskIntoConstraints = false
+        categoryLabel.topAnchor.constraint(equalTo: lineDetailView.topAnchor, constant: 16.0).isActive = true
+        categoryLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 32.0).isActive = true
+        
+        self.detailView.addSubview(categoryValueLabel)
+        categoryValueLabel.translatesAutoresizingMaskIntoConstraints = false
+        categoryValueLabel.topAnchor.constraint(equalTo: categoryLabel.bottomAnchor, constant: 6.0).isActive = true
+        categoryValueLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        
+        self.detailView.addSubview(riskLabel)
+        riskLabel.translatesAutoresizingMaskIntoConstraints = false
+        riskLabel.topAnchor.constraint(equalTo: categoryValueLabel.bottomAnchor, constant: 16.0).isActive = true
+        riskLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 32.0).isActive = true
+
+        self.detailView.addSubview(riskValueLabel)
+        riskValueLabel.translatesAutoresizingMaskIntoConstraints = false
+        riskValueLabel.topAnchor.constraint(equalTo: riskLabel.bottomAnchor, constant: 6.0).isActive = true
+        riskValueLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        
+        //add profile Button
         changeProfileButton.isHidden = true
         addSubview(changeProfileButton)
         changeProfileButton.translatesAutoresizingMaskIntoConstraints = false
@@ -187,8 +343,6 @@ class ToolCell: BaseCell {
         profileView.layer.cornerRadius = 15.0
         profileView.clipsToBounds = true
         
-        profileView.layer.cornerRadius = 6.0
-        profileView.layer.masksToBounds = true
 
         addSubview(profileView)
         profileView.translatesAutoresizingMaskIntoConstraints = false
@@ -292,21 +446,35 @@ class ToolCell: BaseCell {
         
         if isHeightOK , isWeightOk {
             if(people.count > 0) {
-                UIView.animate(withDuration: 0.8, delay: 0, options: [.curveEaseInOut],
+                UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseInOut],
                                animations: {
-                                self.profileView.isHidden = true
-                                self.cmLabel.isHidden = true
-                                self.profileLabel.isHidden = true
-                                self.kgLabel.isHidden = true
-                                self.inputHeightTextfield.isHidden = true
-                                self.desiredWeightTextfield.isHidden = true
-                                self.roundView.isHidden = true
-                                self.enterButton.isHidden = true
-                                self.profileView.layoutIfNeeded()
+                                
+                                self.detailView.alpha = 1
+                                self.changeProfileButton.alpha = 1
+                                
+                                
+                                self.profileView.alpha = 0
+                                self.cmLabel.alpha = 0
+                                self.profileLabel.alpha = 0
+                                self.kgLabel.alpha = 0
+                                self.inputHeightTextfield.alpha = 0
+                                self.desiredWeightTextfield.alpha = 0
+                                self.roundView.alpha = 0
+                                self.enterButton.alpha = 0
+                                self.layoutIfNeeded()
                                 
                 },  completion: {(_ completed: Bool) -> Void in
                     self.detailView.isHidden = false
                     self.changeProfileButton.isHidden = false
+                    
+                    self.profileView.isHidden = true
+                    self.cmLabel.isHidden = true
+                    self.profileLabel.isHidden = true
+                    self.kgLabel.isHidden = true
+                    self.inputHeightTextfield.isHidden = true
+                    self.desiredWeightTextfield.isHidden = true
+                    self.roundView.isHidden = true
+                    self.enterButton.isHidden = true
                 })
                 defaults.set(true, forKey: "wasShowInput")
                 defaults.set(Double(inputHeightTextfield.text!), forKey: "height")
@@ -325,41 +493,53 @@ class ToolCell: BaseCell {
 
     //MARK: - editProfile Button Action
     @objc func editProfileButtonAction() {
-        UIView.animate(withDuration: 0.6, delay: 0, options: [.curveEaseInOut],
+        UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseInOut],
                        animations: {
-                        self.detailView.isHidden = true
-                        self.changeProfileButton.isHidden = true
-                        self.profileView.isHidden = false
-                        self.cmLabel.isHidden = false
-                        self.profileLabel.isHidden = false
-                        self.kgLabel.isHidden = false
-                        self.inputHeightTextfield.isHidden = false
-                        self.desiredWeightTextfield.isHidden = false
-                        self.roundView.isHidden = false
-                        self.enterButton.isHidden = false
-                        self.profileView.layoutIfNeeded()
                         
-        },  completion: {(_ completed: Bool) -> Void in
-            
-        })
+                        self.detailView.alpha = 0
+                        self.changeProfileButton.alpha = 0
+                        
+                        self.profileView.alpha = 1
+                        self.cmLabel.alpha = 1
+                        self.profileLabel.alpha = 1
+                        self.kgLabel.alpha = 1
+                        self.inputHeightTextfield.alpha = 1
+                        self.desiredWeightTextfield.alpha = 1
+                        self.roundView.alpha = 1
+                        self.enterButton.alpha = 1
+                        self.layoutIfNeeded()
+                    
+                        
+            },  completion: {(_ completed: Bool) -> Void in
+                self.detailView.isHidden = true
+                self.changeProfileButton.isHidden = true
+                
+                self.profileView.isHidden = false
+                self.cmLabel.isHidden = false
+                self.profileLabel.isHidden = false
+                self.kgLabel.isHidden = false
+                self.inputHeightTextfield.isHidden = false
+                self.desiredWeightTextfield.isHidden = false
+                self.roundView.isHidden = false
+                self.enterButton.isHidden = false
+                
+                self.desiredWeightTextfield.text! = String(self.defaults.double(forKey: "desizedWeight"))
+                self.inputHeightTextfield.text! = String(self.defaults.double(forKey: "height"))
+                
+            })
     }
     
     func setProfileViewPosition() {
-        UIView.animate(withDuration: 0, delay: 0, options: [.curveEaseInOut],
-                       animations: {
-                        self.profileView.isHidden = true
-                        self.cmLabel.isHidden = true
-                        self.profileLabel.isHidden = true
-                        self.kgLabel.isHidden = true
-                        self.inputHeightTextfield.isHidden = true
-                        self.desiredWeightTextfield.isHidden = true
-                        self.roundView.isHidden = true
-                        self.enterButton.isHidden = true
-                        self.profileView.layoutIfNeeded()
-                        
-        },  completion: {(_ completed: Bool) -> Void in
-            self.detailView.isHidden = false
-            self.changeProfileButton.isHidden = false
-        })
+        self.detailView.isHidden = false
+        self.changeProfileButton.isHidden = false
+        
+        self.profileView.isHidden = true
+        self.cmLabel.isHidden = true
+        self.profileLabel.isHidden = true
+        self.kgLabel.isHidden = true
+        self.inputHeightTextfield.isHidden = true
+        self.desiredWeightTextfield.isHidden = true
+        self.roundView.isHidden = true
+        self.enterButton.isHidden = true
     }
 }
